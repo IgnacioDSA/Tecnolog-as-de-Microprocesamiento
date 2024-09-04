b9cc21870cb8fe83446bf1960a4f1bcdd6023ecb
IgnacioDSA <ignacio.dasilveira@estudiantes.utec.edu.uy>
2024-09-04 16:42:43 -0300

    Update EJ1PART1INVERTIDO.asm
    puerto d y delays

--- EvaluacionContinua1/Codigo/EJ1PART1INVERTIDO.asm
+++ EvaluacionContinua1/Codigo/EJ1PART1INVERTIDO.asm
@@ -2,15 +2,17 @@
 .ORG 0x000
 
 ldi r16, 0xFF 
-out DDRB, r16 ; pines del puerto B como salidas
+out DDRD, r16 ; pines del puerto B como salidas
 
 ldi r17, 0x01 ; posicion inicial LED
 
 main_loop:
 	mov r16, r17 ; copiamos la posicion actual del led
 	com r16      ; complementamos el valor para encender el LED (invertir los bits)
-	out PORTB, r16 ; encendemos el led en el pin correspondiente
+	out PORTD, r16 ; encendemos el led en el pin correspondiente
 
+	ldi r20, 0x08
+delayyy:
 	ldi r18, 0xFF ; valor para el segundo retardo
 delayy:
 	ldi r19, 0xFF ; valor para el primer retardo 
@@ -19,9 +21,11 @@ delay:
 	brne delay ; repetir hasta que sea 0
 	dec r18 ; disminuir el contador del segundo retardo
 	brne delayy ; repetir hasta que sea 0
+	dec r20
+	brne delayyy
 
 	ldi r16, 0xFF ; poner todos los LEDs en estado apagado (1)
-	out PORTB, r16 ; apagar los pines del puerto B
+	out PORTD, r16 ; apagar los pines del puerto B
 
 	lsl r17 ; desplazamos el LED
 	cpi r17, 0x00 ; comparamos si llegamos al final del puerto

3955e56e78cc087a5429160a216c842509a9d952
braian <braian.arias@estudiantes.utec.edu.uy>
2024-09-05 13:58:34 -0300

    Update EJ2-CaraTriste.asm
    añadir comentarios forma de la estructura

--- EvaluacionContinua1/Codigo/EJ2-CaraTriste.asm
+++ EvaluacionContinua1/Codigo/EJ2-CaraTriste.asm
@@ -2,103 +2,96 @@
 .ORG 0x000
 
 ldi r17, 0xFF       ; Configuramos todos los pines del puerto D como salidas
-ldi r18, 0xFF;
-OUT DDRD, r17     
-OUT DDRB, r18
+ldi r18, 0xFF       ; Configuramos todos los pines del puerto B como salidas
+OUT DDRD, r17       ; Escribimos el valor 0xFF en DDRD para configurar todos los pines de D como salidas
+OUT DDRB, r18       ; Escribimos el valor 0xFF en DDRB para configurar todos los pines de B como salidas
+
 main_loop:
 
+    ; Encender LEDs 1
+    ldi r16, 0xFF   ; 0b11111111: Todos los LEDs apagados (ya que 1 = apagado)
+    out PORTD, r16  ; Enviamos el valor a PORTD para apagar todos los LEDs
+    rcall delay     ; Llamamos a la subrutina delay
+    ldi r16, 0xFF   ; Apagamos nuevamente todos los LEDs
+    ldi r18, 0x01   ; Seleccionamos la fila 1 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
-
-
-    ; Encender LEDs 1  (bajar los pines correspondientes)
-    ldi r16, 0xFF   ; 0b01111110: LED 1 y LED 8 encendidos (el resto apagado)
+    ; Encender LEDs 2
+    ldi r16, 0xD7   ; Configuramos los pines de PORTD para encender algunos LEDs
     out PORTD, r16  ; Enviamos el valor a PORTD
     rcall delay     ; Llamamos a la subrutina delay
-    ldi r16, 0xFF   ; Apagamos todos los LEDs (poner todos los pines en 1)
-	ldi r18, 0x01
-    out PORTB, r18
-    rcall delay
-    
-    ; Encender LEDs 2 
-    ldi r16, 0xD7   
-    out PORTD, r16
-    rcall delay
     ldi r16, 0xFF   ; Apagamos todos los LEDs
-    out PORTD, r16
-	ldi r18, 0x02
-    out PORTB, r18
-    rcall delay
-    
-    ; Encender LEDs 3 
-    ldi r16, 0xD7   
-    out PORTD, r16
-    rcall delay
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x02   ; Seleccionamos la fila 2 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
+
+    ; Encender LEDs 3
+    ldi r16, 0xD7   ; Configuramos los pines de PORTD para encender algunos LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    rcall delay     ; Llamamos a la subrutina delay
     ldi r16, 0xFF   ; Apagamos todos los LEDs
-    out PORTD, r16
-	ldi r18, 0x03
-    out PORTB, r18
-    rcall delay
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x03   ; Seleccionamos la fila 3 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
     ; Encender LEDs 4
-    ldi r16, 0xFF   
-    out PORTD, r16
-    rcall delay
     ldi r16, 0xFF   ; Apagamos todos los LEDs
-    out PORTD, r16
-	ldi r18, 0x04
-    out PORTB, r18
-    rcall delay
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    rcall delay     ; Llamamos a la subrutina delay
+    ldi r16, 0xFF   ; Apagamos todos los LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x04   ; Seleccionamos la fila 4 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
-    ; Encender LEDs 5    
-	ldi r16, 0xFF   
-    out PORTD, r16
-    rcall delay
+    ; Encender LEDs 5
     ldi r16, 0xFF   ; Apagamos todos los LEDs
-    out PORTD, r16
-	ldi r18, 0x05   ;fila
-    out PORTB, r18
-    rcall delay
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    rcall delay     ; Llamamos a la subrutina delay
+    ldi r16, 0xFF   ; Apagamos todos los LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x05   ; Seleccionamos la fila 5 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
     ; Encender LEDs 6
-    ldi r16, 0xC7   
-    out PORTD, r16
-    rcall delay
+    ldi r16, 0xC7   ; Configuramos los pines de PORTD para encender algunos LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    rcall delay     ; Llamamos a la subrutina delay
     ldi r16, 0xFF   ; Apagamos todos los LEDs
-    out PORTD, r16
-	ldi r18, 0x06   ;fila
-    out PORTB, r18
-    rcall delay
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x06   ; Seleccionamos la fila 6 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
-    ; Encender LEDs 7 (bajar los pines correspondientes)
-    ldi r16, 0xBB   
+    ; Encender LEDs 
+    ldi r16, 0xBB   ; Configuramos los pines de PORTD para encender algunos LEDs
     out PORTD, r16  ; Enviamos el valor a PORTD
     rcall delay     ; Llamamos a la subrutina delay
-    ldi r16, 0xFF   ; Apagamos todos los LEDs (poner todos los pines en 1)
-    out PORTD, r16
-	ldi r18, 0x07   ;fila
-    out PORTB, r18
-    rcall delay
+    ldi r16, 0xFF   ; Apagamos todos los LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x07   ; Seleccionamos la fila 7 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
 
-	; Encender LEDs 0
-	ldi r16, 0xFF   ; columna
+   ; Encender LEDs 0
+    ldi r16, 0xFF   ; Apagamos todos los LEDs
     out PORTD, r16  ; Enviamos el valor a PORTD
     rcall delay     ; Llamamos a la subrutina delay
-    ldi r16, 0xFF   ; Apagamos todos los LEDs (poner todos los pines en 1)
-    out PORTD, r16
-	ldi r18, 0x00   ;fila
-    out PORTB, r18
-    rcall delay
+    ldi r16, 0xFF   ; Apagamos todos los LEDs
+    out PORTD, r16  ; Enviamos el valor a PORTD
+    ldi r18, 0x00   ; Seleccionamos la fila 0 (configurando el puerto B)
+    out PORTB, r18  ; Enviamos el valor a PORTB
+    rcall delay     ; Llamamos a la subrutina delay
+
     rjmp main_loop  ; Volvemos al bucle principal
 
 delay:
-	ldi r21, 0x08
-	;ldi r20, 0xFF
-    ;ldi r19, 0xFF   ; Inicializamos r19 con 0xFF para el contador de retardo
+	ldi r21, 0x08   ; Cargamos un valor en r21 para crear el retardo
 delay_loop:
-    ;dec r19         ; Disminuimos el contador de retardo
-    ;brne delay_loop ; Repetimos hasta que r19 sea 0
-    ;dec r20
-	;brne delay_loop
-	dec r21
-    brne delay_loop
+	dec r21         ; Disminuimos el valor de r21
+    brne delay_loop     ; Si r21 no es 0, repetimos el ciclo
 	ret             ; Retornamos de la subrutina delay

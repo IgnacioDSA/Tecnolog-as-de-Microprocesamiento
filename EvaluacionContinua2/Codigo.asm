e2d5c707bec3e8744a1bd18ec8d2b330534c2bd0
IgnacioDSA <ignacio.dasilveira@estudiantes.utec.edu.uy>
2024-09-06 20:31:40 -0300

    Create Codigo.asm

---
+++ EvaluacionContinua2/Codigo.asm
@@ -0,0 +1,24 @@
+.include "m328pdef.inc"
+
+.org 0x00
+ldi r16, 0xFF
+ldi r17, (0<<PB5)
+out DDRD, r16
+out DDRB, r17
+
+main_loop:
+
+sbis PIND, PB5
+ldi r18, 0b10111111
+out PORTD, r18
+sbic PIND, PB5
+ldi r18, 0b01100000
+out PORTD, r18
+sbis PIND, PB5
+sbic PIND, PB5
+ldi r18, 0b11011010
+out PORTD, r18
+sbis PIND, PB5
+sbic PIND, PB5
+
+rjmp main_loop
